ed11c7a15e28943cad0f78e75ebc3ae6c789939e
abldmr <aaron.baldomar@gmail.com>
2024-04-08 15:03:52 +0800

    first commit

--- js/speedPotions.js
+++
@@ -1,8 +0,0 @@
-{ // SpeedPots
-SpeedPotName = [
-["Нет зелья","None"]
-,["Зелье концентрации","Concentration Potion"]
-,["Зелье пробуждения","Awakening Potion"]
-,["Зелье бесстрашия","Berserk Potion"]
-];
-}
